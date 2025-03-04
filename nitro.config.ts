c8ea1a78e61517a80708a4442b317993b424ae9c
脆皮丶 <cuiqg@foxmail.com>
2025-03-04 23:44:29 +0800

    修改部分信息

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,11 +1,14 @@
 import { createRequire } from "node:module"
+import { defineNitroConfig } from "nitropack/config"
 
 const nitroPkg = createRequire(import.meta.url)("nitropack/package.json")
 
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   preset: 'vercel',
-  srcDir: "server",
+  srcDir: "./server",
+  baseURL: "/",
+  timing: true,
   routeRules: {
     ".*": {
       cors: true
@@ -19,6 +22,9 @@ export default defineNitroConfig({
   },
   runtimeConfig: {
     nitroVersion: nitroPkg.version,
+  },
+  vercel: {
+    config: {}
   },
   compatibilityDate: "2025-03-04"
 });
